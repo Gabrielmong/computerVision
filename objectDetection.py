6541ff4a45e57788ff9eb684ac9e41c755e6823b
Gabrielmong <gabriel.monge.lizano@gmail.com>
2023-06-18 19:19:18 -0600

    feat: pick files, basic gui

--- objectDetection.py
+++ objectDetection.py
@@ -1,7 +1,10 @@
+
 import cv2
 import numpy as np
 import time
 import os
+import tkinter as tk
+from tkinter import filedialog
 
 
 class Detector:
@@ -27,36 +30,28 @@ class Detector:
 
         self.colors = np.random.uniform(0, 255, size=(len(self.classes), 3))
 
-    def onVideo(self):
-        cap = cv2.VideoCapture(self.videoPath)
+    def processFrame(self, frame):
+        height, width, _ = frame.shape
 
-        if not cap.isOpened():
-            print("Cannot open video")
-            exit()
+        aspectRatio = width / height
 
-        (success, frame) = cap.read()
+        if width > 800:
+            newWidth = 800
+            newHeight = newWidth / aspectRatio
 
-        startTime = time.time()
+            frame = cv2.resize(frame, (int(newWidth), int(newHeight)))
 
-        while success:
-            currentTime = time.time()
-            if (currentTime - startTime) > 0:
-                fps = 1 / (currentTime - startTime)
-            else:
-                fps = 0
+        classLabelIDs, confidences, bbox = self.net.detect(
+            frame, confThreshold=0.5)
 
-            startTime = currentTime + 0.00001
+        bboxs = list(bbox)
+        confidences = list(np.array(confidences).reshape(1, -1)[0])
+        confidences = list(map(float, confidences))
 
-            classLabelIDs, confidences, bbox = self.net.detect(
-                frame, confThreshold=0.5)
+        bboxIdx = cv2.dnn.NMSBoxes(
+            bbox, confidences, score_threshold=0.5, nms_threshold=0.2)
 
-            bboxs = list(bbox)
-            confidences = list(np.array(confidences).reshape(1, -1)[0])
-            confidences = list(map(float, confidences))
-
-            bboxIdx = cv2.dnn.NMSBoxes(
-                bbox, confidences, score_threshold=0.5, nms_threshold=0.2)
-
+        try:
             if len(bboxIdx != 0):
                 for i in range(0, len(bboxIdx)):
 
@@ -74,7 +69,8 @@ class Detector:
 
                     cv2.rectangle(frame, (x, y), (x + w, y + h),
                                   color=classColor, thickness=1)
-                    cv2.putText(frame, displayStr, (x, y - 10),
+
+                    cv2.putText(frame, displayStr, (x, y - 15),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, classColor, 2)
 
                     lineWidth = min(int(w * 0.3), int(h * 0.3))
@@ -89,23 +85,54 @@ class Detector:
                     cv2.line(frame, (x + w, y), (x + w, y + lineWidth),
                              classColor, thickness=3)
 
-                    cv2.line(frame, (x, y + h), (x + lineWidth,
-                             y + h), classColor, thickness=3)
+                    cv2.line(frame, (x, y + h), (x + lineWidth, y + h),
+                             classColor, thickness=3)
                     cv2.line(frame, (x, y + h), (x, y + h - lineWidth),
                              classColor, thickness=3)
 
-                    cv2.line(frame, (x + w, y + h), (x + w -
-                             lineWidth, y + h), classColor, thickness=3)
+                    cv2.line(frame, (x + w, y + h),
+                             (x + w - lineWidth, y + h
+                              ), classColor, thickness=3)
                     cv2.line(frame, (x + w, y + h), (x + w, y +
-                             h - lineWidth), classColor, thickness=3)
+                                                     h - lineWidth), classColor, thickness=3)
+
+            return frame
+
+        except:
+            return frame
+
+    def onVideo(self):
+        cap = cv2.VideoCapture(self.videoPath)
+
+        if not cap.isOpened():
+            print("Cannot open video")
+            return None
+
+        (success, frame) = cap.read()
+
+        startTime = time.time()
+
+        while success:
+            currentTime = time.time()
+            if (currentTime - startTime) > 0:
+                fps = 1 / (currentTime - startTime)
+            else:
+                fps = 0
+
+            startTime = currentTime + 0.00001
+
+            frame = self.processFrame(frame)
 
             cv2.putText(frame, 'FPS: {:.2f}'.format(
                 fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            cv2.imshow("Output", frame)
+            cv2.imshow("Output, press Q to exit", frame)
 
             key = cv2.waitKey(1) & 0xFF
 
             if key == ord("q"):
+                break
+
+            if cv2.getWindowProperty("Output, press Q to exit", cv2.WND_PROP_VISIBLE) < 1:
                 break
 
             (success, frame) = cap.read()
@@ -114,78 +141,81 @@ class Detector:
 
     def classifyImage(self, imagePath):
         image = cv2.imread(imagePath)
+        image = self.processFrame(image)
 
-        classLabelIDs, confidences, bbox = self.net.detect(
-            image, confThreshold=0.5)
+        cv2.imshow("Output, press Q to exit", image)
 
-        bboxs = list(bbox)
-        confidences = list(np.array(confidences).reshape(1, -1)[0])
-        confidences = list(map(float, confidences))
+        while True:
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
 
-        bboxIdx = cv2.dnn.NMSBoxes(
-            bbox, confidences, score_threshold=0.5, nms_threshold=0.2)
+            if cv2.getWindowProperty("Output, press Q to exit", cv2.WND_PROP_VISIBLE) < 1:
+                break
 
-        if len(bboxIdx != 0):
-            for i in range(0, len(bboxIdx)):
-
-                bbox = bboxs[np.squeeze(bboxIdx[i])]
-                classConfidence = confidences[np.squeeze(bboxIdx[i])]
-                classLabelID = np.squeeze(
-                    classLabelIDs[np.squeeze(bboxIdx[i])])
-                classLabel = self.classes[classLabelID]
-                classColor = [int(c) for c in self.colors[classLabelID]]
-
-                displayStr = '{}: {:.2f}'.format(classLabel, classConfidence)
-
-                x, y, w, h = bbox
-
-                cv2.rectangle(image, (x, y), (x + w, y + h),
-                              color=classColor, thickness=1)
-                cv2.putText(image, displayStr, (x, y - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, classColor, 2)
-
-                lineWidth = min(int(w * 0.3), int(h * 0.3))
-
-                cv2.line(image, (x, y), (x + lineWidth, y),
-                         classColor, thickness=3)
-                cv2.line(image, (x, y), (x, y + lineWidth),
-                         classColor, thickness=3)
-
-                cv2.line(image, (x + w, y), (x + w - lineWidth, y),
-                         classColor, thickness=3)
-                cv2.line(image, (x + w, y), (x + w, y + lineWidth),
-                         classColor, thickness=3)
-
-                cv2.line(image, (x, y + h), (x + lineWidth, y + h),
-                         classColor, thickness=3)
-                cv2.line(image, (x, y + h), (x, y + h - lineWidth),
-                         classColor, thickness=3)
-
-                cv2.line(image, (x + w, y + h),
-                         (x + w - lineWidth, y + h), classColor, thickness=3)
-                cv2.line(image, (x + w, y + h), (x + w, y +
-                         h - lineWidth), classColor, thickness=3)
-
-        cv2.imshow("Output", image)
-        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 def main():
-    # 0 nikon camera
-    # 3 logitech capture
-    videoPath = 0
-
-    imagePath = os.path.join("images", "city.jpg")
-
+    os.system("cls")
+    print("Object Detection using OpenCV")
     configPath = os.path.join(
         "model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
     modelPath = os.path.join("model_data", "frozen_inference_graph.pb")
     classesPath = os.path.join("model_data", "coco.names")
 
     detector = Detector(None, configPath, modelPath, classesPath)
-    # detector.onVideo()
 
-    detector.classifyImage(imagePath)
+    root = tk.Tk()
+    root.title("Object Detection")
+    root.geometry("300x70")
+    root.eval('tk::PlaceWindow . center')
+
+    def openVideo():
+        root.destroy()
+
+        root2 = tk.Tk()
+        root2.title("Video")
+        root2.geometry("300x70")
+        root2.eval('tk::PlaceWindow . center')
+
+        def openCamera():
+            detector = Detector(0, configPath, modelPath, classesPath)
+            detector.onVideo()
+
+        def openFile():
+            root2.filename = filedialog.askopenfilename(
+                initialdir="/", title="Select file", filetypes=(("mp4 files", "*.mp4"), ("all files", "*.*")))
+            detector = Detector(root2.filename,
+                                configPath, modelPath, classesPath)
+            detector.onVideo()
+
+        def backToMain():
+            root2.destroy()
+            main()
+
+        cameraButton = tk.Button(root2, text="Camera", command=openCamera)
+        cameraButton.pack()
+
+        fileButton = tk.Button(root2, text="File", command=openFile)
+        fileButton.pack()
+
+        root2.protocol("WM_DELETE_WINDOW", backToMain)
+
+        root2.mainloop()
+
+    def openImage():
+        root.filename = filedialog.askopenfilename(
+            initialdir="/", title="Select file", filetypes=(("jpeg files", "*.jpg;*.jpeg"), ("all files", "*.*")))
+        detector.classifyImage(root.filename)
+
+    videoButton = tk.Button(root, text="Video", command=openVideo)
+    videoButton.pack()
+
+    imageButton = tk.Button(root, text="Image", command=openImage)
+    imageButton.pack()
+
+    root.mainloop()
 
 
 if __name__ == "__main__":
